61e8c0836e983b4d90b446ca6b2407d1c7df35cb
Burke <beatty.burke@gmail.com>
2021-05-06 09:01:43 -0400

    added api key and search results variables

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var APIkey = 'c3af6e45795e59cae1dc9209a98b58e5'
+
+var CityName = document.getElementById('searchCity')
+var CityTemp = document.getElementById('Temp')
+var CityWind = document.getElementById('Wind')
+var CityHumidity = document.getElementById('Humidity')
+var CityUV = document.getElementById('UVindex')
+
+
+
+
