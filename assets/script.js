d2eeabc2237063044ed9958be22ccd80c3d3b7c2
Burke <beatty.burke@gmail.com>
2021-05-06 11:30:38 -0400

    first api call

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,28 @@
 var APIkey = 'c3af6e45795e59cae1dc9209a98b58e5'
 
-var CityName = document.getElementById('searchCity')
-var CityTemp = document.getElementById('Temp')
-var CityWind = document.getElementById('Wind')
-var CityHumidity = document.getElementById('Humidity')
-var CityUV = document.getElementById('UVindex')
+var cityName = document.getElementById('searchCity')
+var cityTemp = document.getElementById('Temp')
+var cityWind = document.getElementById('Wind')
+var cityHumidity = document.getElementById('Humidity')
+var cityUV = document.getElementById('UVindex')
+var searchBtn = document.getElementById('searchBtn')
 
 
-function cityWeather()
+function cityWeather() {
+    var searchInput = document.getElementById('searchInput').value
+    console.log(searchInput)
 
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + searchInput + '&appid=' + APIkey)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+    
+
+}
+
+searchBtn.addEventListener('click', cityWeather)
+
+// api.openweathermap.org/data/2.5/forecast?q=wilmington&appid=c3af6e45795e59cae1dc9209a98b58e5
